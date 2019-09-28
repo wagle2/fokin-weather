f9f93b2791b3830895eec7d7295f453d2aa602a6
wagle2 <archmage.maple@gmail.com>
2019-09-28 10:15:36 +0900

    Finished!

---
+++ Weather.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import Prototypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const weatherOptions = {
+    Thunderstorm: {
+      iconName: "weather-lightning",
+      gradient: ["#373B44", "#4286f4"]
+    },
+    Drizzle: {
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"]
+    },
+    Rain: {
+      iconName: "weather-rainy",
+      gradient: ["#00C6FB", "#005BEA"]
+    },
+    Snow: {
+      iconName: "weather-snowy",
+      gradient: ["#7DE2FC", "#B9B6E5"]
+    },
+    Atmosphere: {
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"]
+    },
+    Clear: {
+      iconName: "weather-sunny",
+      gradient: ["#FF7300", "#FEF253"]
+    },
+    Clouds: {
+      iconName: "weather-cloudy",
+      gradient: ["#D7D2CC", "#304352"]
+    },
+    Mist: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Dust: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Haze: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Haze",
+      subtitle: "Just don't go outside."
+    }
+  };
+  
+
+
+export default function Weather({ temp, condition }) {
+  return (
+    <LinearGradient
+      colors={weatherOptions["Haze"].gradient}
+      style={styles.container}>
+      <StatusBar barStyle="light-content"/>
+      <View style={styles.halfContainer}>
+      <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions["Haze"].iconName}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp} º</Text>
+      </View>
+      <View style={{...styles.halfContainer, ...styles.textContainer}}>
+        <Text style={styles.title}>{weatherOptions["Haze"].title}</Text>
+        <Text style={styles.subtitle}>{weatherOptions["Haze"].subtitle}</Text>
+        </View>
+      </LinearGradient>
+  );
+}
+
+Weather.protoTypes = {
+  temp: Prototypes.number.isRequired,
+  condition: Prototypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust"
+  ]).isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 42,
+    color: "white"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+      color: "white",
+      fontSize: 44,
+      fontWeight: "300",
+      marginBottom: 10
+  },
+  subtitle: {
+      fontWeight: "600",
+      color: "white",
+      fontSize: 24
+  },
+  textContainer: {
+      paddingHorizontal: 20,
+      alignItems: "flex-start"
+  }
+});
